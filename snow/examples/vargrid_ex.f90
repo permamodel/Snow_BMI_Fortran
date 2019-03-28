9a7ca4e40a726e866a91cd96938c5e66f8644b60
wk1984 <waccount1984@gmail.com>
2019-03-28 17:11:03 -0600

    add vargrid_ex

--- snow/examples/vargrid_ex.f90
+++ snow/examples/vargrid_ex.f90
@@ -2,10 +2,10 @@
 program vargrid_test
 
   use bmif, only: BMI_MAX_VAR_NAME
-  use bmiheatf
+  use bmisnowf
   implicit none
 
-  type (bmi_heat) :: m
+  type (bmi_snow) :: m
   integer :: s, i
   character (len=BMI_MAX_VAR_NAME), pointer :: names(:)
   integer :: grid_id
@@ -15,39 +15,39 @@ program vargrid_test
   integer, dimension(2) :: iarray
 
   write (*,"(a)",advance="no") "Initializing..."
-  s = m%initialize("")
+  s = m%initialize("test1.cfg")
   write (*,*) "Done."
 
   s = m%get_output_var_names(names)
-  write (*,"(a30)") "Output variables:"
+  write (*,"(a)") "Output variables:"
   do i = 1, size(names)
-     write (*,"(a30, 1x, a)") "-", names(i)
+     write (*,"(a, 1x, a)") "-", trim(names(i))
   end do
 
   s = m%get_var_grid(names(1), grid_id)
-  write (*,"(a30, i3)") "Grid id:", grid_id
+  write (*,"(a, i3)") "Grid id:", grid_id
 
   s = m%get_grid_type(grid_id, astring)
-  write (*,"(a30, 1x, a30)") "Grid type:", astring
+  write (*,"(a, 1x, a)") "Grid type:", trim(astring)
   s = m%get_grid_origin(grid_id, darray)
-  write (*,"(a30, 1x, 2(f8.2))") "Grid origin:", darray
+  write (*,"(a, 1x, 2(f8.2))") "Grid origin:", darray
   s = m%get_grid_rank(grid_id, asize)
-  write (*,"(a30, i3)") "Grid rank:", asize
+  write (*,"(a, i3)") "Grid rank:", asize
   s = m%get_grid_shape(grid_id, iarray)
-  write (*,"(a30, 2(1x, i3))") "Grid shape:", iarray
+  write (*,"(a, 2(1x, i3))") "Grid shape:", iarray
   s = m%get_grid_size(grid_id, asize)
-  write (*,"(a30, i8)") "Grid size:", asize
+  write (*,"(a, i8)") "Grid size:", asize
   s = m%get_grid_spacing(grid_id, darray)
-  write (*,"(a30, 1x, 2(f8.2))") "Grid spacing:", darray
+  write (*,"(a, 1x, 2(f8.2))") "Grid spacing:", darray
 
   s = m%get_var_itemsize(names(1), asize)
-  write (*,"(a30, i8, 1x, a)") "Item size:", asize, "bytes"
+  write (*,"(a, i8, 1x, a)") "Item size:", asize, "bytes"
   s = m%get_var_nbytes(names(1), asize)
-  write (*,"(a30, i8, 1x, a)") "Variable size:", asize, "bytes"
+  write (*,"(a, i8, 1x, a)") "Variable size:", asize, "bytes"
   s = m%get_var_type(names(1), astring)
-  write (*,"(a30, 1x, a30)") "Variable type:", astring
+  write (*,"(a, 1x, a)") "Variable type:", trim(astring)
   s = m%get_var_units(names(1), astring)
-  write (*,"(a30, 1x, a30)") "Variable units:", astring
+  write (*,"(a, 1x, a)") "Variable units:", trim(astring)
 
   write (*,"(a)", advance="no") "Finalizing..."
   s = m%finalize()
