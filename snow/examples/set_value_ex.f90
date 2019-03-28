a54e3c8c631341072d0a9b1c6fa23b0b3a813924
wk1984 <waccount1984@gmail.com>
2019-03-28 16:36:55 -0600

    add set_value_ex

--- snow/examples/set_value_ex.f90
+++ snow/examples/set_value_ex.f90
@@ -2,11 +2,11 @@
 program set_value_ex
 
   use bmif, only: BMI_MAX_VAR_NAME
-  use bmiheatf
+  use bmisnowf
   use testing_helpers
   implicit none
 
-  type (bmi_heat) :: m
+  type (bmi_snow) :: m
   integer :: s, i, j, grid_id
   character (len=BMI_MAX_VAR_NAME), pointer :: names(:)
   integer :: grid_size, dims(2), locations(3)
@@ -15,12 +15,14 @@ program set_value_ex
   character(len=30) :: rowfmt
 
   write (*,"(a)",advance="no") "Initializing..."
-  s = m%initialize("")
+  s = m%initialize("test1.cfg")
   write (*,*) "Done."
 
   s = m%get_output_var_names(names)
-  write (*,"(a, a)") "Output variables: ", names
-
+  do i = 1, 2
+  write (*,"(a, i, ': ', a)") "Output variables ", i, trim(names(i))
+  enddo
+  
   s = m%get_var_grid(names(1), grid_id)
   s = m%get_grid_shape(grid_id, dims)
   write(rowfmt,'(a,i4,a)') '(', dims(2), '(1x,f6.1))'
@@ -29,28 +31,18 @@ program set_value_ex
 
   write (*, "(a)") "Initial values:"
   allocate(z(grid_size))
-  s = m%get_value("plate_surface__temperature", z)
-  call print_array(z, dims)
-
+  s = m%get_value("land_surface_air__temperature", z)
+  print *, z
+  
   write (*,"(a)",advance="no") "Setting new values..."
   z = 42.0
-  s = m%set_value("plate_surface__temperature", z)
+  s = m%set_value("land_surface_air__temperature", [z])
   write (*,*) "Done."
   write (*, "(a)") "New values:"
   allocate(y(grid_size))
-  s = m%get_value("plate_surface__temperature", y)
-  call print_array(y, dims)
-
-  write (*, "(a)") "Set values at three locations:"
-  locations = [21, 41, 62]
-  values = [-1.0, -1.0, -1.0]
-  write (*,*) "Locations: ", locations
-  write (*,*) "Values: ", values
-  s = m%set_value_at_indices("plate_surface__temperature", locations, values)
-  write (*, "(a)") "New values:"
-  s = m%get_value("plate_surface__temperature", y)
-  call print_array(y, dims)
-
+  s = m%get_value("land_surface_air__temperature", y)
+  print *, y
+  
   write (*,"(a)", advance="no") "Finalizing..."
   deallocate(z, y)
   s = m%finalize()
