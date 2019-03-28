2175c08935fdcbf2e3d7e8ba388b4a4c79e3de35
wk1984 <waccount1984@gmail.com>
2019-03-28 11:52:16 -0600

    add test_get_end_time and test_get_grid_size

---
+++ snow/tests/test_get_grid_size.f90
@@ -0,0 +1,22 @@
+program test_get_grid_size
+
+  use bmif, only: BMI_FAILURE
+  use bmisnowf
+  use fixtures, only: config_file, status
+
+  implicit none
+
+  integer, parameter :: grid_id = 0
+  integer, parameter :: expected_size = 1
+
+  type (bmi_snow) :: m
+  integer :: grid_size
+
+  status = m%initialize(config_file)
+  status = m%get_grid_size(grid_id, grid_size)
+  status = m%finalize()
+
+  if (grid_size.ne.expected_size) then
+     stop BMI_FAILURE
+  end if
+end program test_get_grid_size
