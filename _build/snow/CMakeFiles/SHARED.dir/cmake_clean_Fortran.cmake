e54016d46d5b865024967389831cb66129d1956c
wk1984 <waccount1984@gmail.com>
2019-03-28 11:15:58 -0600

    change mod names from heat to snow

---
+++ _build/snow/CMakeFiles/SHARED.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,10 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "../mod/bmisnowf.mod"
+  "../mod/BMISNOWF.mod"
+  "CMakeFiles/SHARED.dir/bmisnowf.mod.stamp"
+
+  "../mod/snow_model.mod"
+  "../mod/SNOW_MODEL.mod"
+  "CMakeFiles/SHARED.dir/snow_model.mod.stamp"
+  )
--- snow/snow_model.pc.cmake
+++ snow/snow_model.pc.cmake
@@ -1,5 +1,5 @@
 Name: snow
 Description: An example of the heat equation, with a BMI, in Fortran
 Version: ${bmi_version}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -l${bmiheat_lib} -l${bmi_lib}
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -l${bmisnow_lib} -l${bmi_lib}
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include -std=f2003
